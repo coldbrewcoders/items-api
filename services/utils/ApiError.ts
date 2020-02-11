d81e215a4fe22ca18a0a19891cc122d7f28b156a
Malcolm Navarro <mnavarro@templuminc.com>
2020-02-10 22:35:05 -0600

    Added handling for Object as message field for ApiError classs to convert to string

--- services/utils/ApiError.ts
+++ services/utils/ApiError.ts
@@ -1,6 +1,5 @@
 import { CustomError } from "ts-custom-error";
 
-
 class ApiError extends CustomError {
 
   // Additional member variables
@@ -10,7 +9,17 @@ class ApiError extends CustomError {
 
   constructor(message: string | Object, httpStatusCode: number = 500, context?: any) {
 
-    super(message);
+    let serializedMessage: string;
+
+    // If the message is not a string, stringify it to work with base Error class
+    if (typeof(message) !== "string") {
+      serializedMessage = JSON.stringify(message);
+    } else {
+      serializedMessage = message as string;
+    }
+
+    // Call constructor of base Error class
+    super(serializedMessage);
 
     // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
@@ -18,7 +27,7 @@ class ApiError extends CustomError {
     }
 
     // Assign constructor params to class fields
-    this.message = message;
+    this.message = serializedMessage;
     this.code = httpStatusCode;
     this.context = context;
     this.date = new Date();
