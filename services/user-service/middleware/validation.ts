5e9ed81e56e131bd4412590c73317932906860e5
Malcolm Navarro <mnavarro@templuminc.com>
2020-02-10 22:34:19 -0600

    Added types to validation middleware function

--- services/user-service/middleware/validation.ts
+++ services/user-service/middleware/validation.ts
@@ -1,17 +1,18 @@
 import { validationResult, matchedData } from "express-validator";
 import HttpStatus from "http-status-codes";
 
-// Utils
-import ApiError from "../../utils/ApiError";
-
 // Types
 import { Request, Response, NextFunction } from "express";
+import { ValidationError, Result } from "express-validator";
+
+// Utils
+import ApiError from "../../utils/ApiError";
 
 
 const validationCheck = (req: Request, _res: Response, next: NextFunction): void => {
 
   // Check request for validation errors
-  const validationErrors = validationResult(req);
+  const validationErrors: Result<ValidationError> = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
     // If there are validation errors, return 400 status
@@ -20,6 +21,7 @@ const validationCheck = (req: Request, _res: Response, next: NextFunction): void
 
   // If there are no validation errors, proceed
   req.matchedData = matchedData(req);
+
   next();
 }
 
