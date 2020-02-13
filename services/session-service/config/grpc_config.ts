6b9cd8db1fc1a85a74fbe57e46a359256b1ec0a9
Malcolm Navarro <mnavarro@templuminc.com>
2020-02-12 19:31:07 -0600

    Made a start server function to be async and have an await on bind so we can handle server start/error better

--- services/session-service/config/grpc_config.ts
+++ services/session-service/config/grpc_config.ts
@@ -6,32 +6,39 @@ import path from "path";
 import { Server } from "grpc";
 import { PackageDefinition } from "@grpc/proto-loader";
 
-// Get path to proto file
-const PROTO_PATH: string = path.join(__dirname, "../../../protos/session.proto");
-
-// Load proto file
-const sessionPackageDefinition: PackageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true });
-
-// Get proto package definition
-// @ts-ignore gRPC proto file is dynamically imported, definition is not generated till runtime
-const loadedSessionGrpcPackage = grpc.loadPackageDefinition(sessionPackageDefinition).session;
-
-// Start gRPC server
-const server: Server = new Server();
-
 // Import gRPC method implementations
 import { validateSession, createSession, replaceSession, removeSession } from "../grpc/session";
 
-// Configure auth service grpc methods
-// @ts-ignore
-server.addService(loadedSessionGrpcPackage.SessionService.service, { validateSession, createSession, replaceSession, removeSession });
 
-// Bind gRPC server to url
-server.bind(
+// Get path to proto file
+const PROTO_PATH: string = path.join(__dirname, "../../../protos/session.proto");
+
+const startServer = async (): Promise<void> => {
+  // Load proto file
+  const sessionPackageDefinition: PackageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true });
+
+  // Get proto package definition
+  // @ts-ignore gRPC proto file is dynamically imported, definition is not generated till runtime
+  const loadedSessionGrpcPackage = grpc.loadPackageDefinition(sessionPackageDefinition).session;
+
+  // Start gRPC server
+  const server: Server = new Server();
+
+  // Configure auth service grpc methods
+  // @ts-ignore
+  server.addService(loadedSessionGrpcPackage.SessionService.service, { validateSession, createSession, replaceSession, removeSession });
+
+  // Bind gRPC server to url
+  await server.bindAsync(
     process.env.SESSION_SERVICE_GRPC_BIND_URL,
     grpc.ServerCredentials.createInsecure(),
-    // (error: Error, port: number) => error ? console.log(error) : console.log(`Session gRPC Server listening on port: ${ port }`)
-);
+    (error: Error, port: number) => error ? console.log(error) : console.log(`Session gRPC Server listening on port: ${ port }`)
+  );
 
-// Start gRPC server
-server.start();
+  // Start gRPC server
+  server.start();
+}
+
+
+// Call start server function
+startServer();
