641c6e9583325c34122578295f9f4193a160fdbd
Malcolm Navarro <mnavarro@templuminc.com>
2020-03-03 10:03:54 -0600

    Updated seed file for users and items

--- DB/seeds/add_items.ts
+++ DB/seeds/add_items.ts
@@ -1,42 +1,40 @@
 import Knex from "knex";
 import faker from "faker";
 
-const createFakeItem = (userId: number) => ({
-  name: faker.commerce.productName(),
-  description: faker.commerce.product(),
-  created_by_user_id: userId,
-  last_modified_by_user_id: userId
-});
+// Types & helpers
+import { IUser, createUser } from "./add_users";
 
-const seed = async (knex: Knex): Promise<void> => {
+export interface IItem {
+  id?: number;
+  name: string;
+  description: string;
+  created_by_user_id: number;
+  last_modified_by_user_id: number;
+}
 
+export const seed = async (knex: Knex): Promise<void> => {
+
+  // Select first user to ensure one exists to satisfy items table foreign key constraint
   const userId: number = 1;
-
-  // Check if there is a user in the database
-  const users = await knex("user_service.users").where("id", userId);
+  const user: IUser = await knex<IUser>("user_service.users").where("id", userId).first();
 
   // If there are no users in the DB, then add one so that items entry can reference a user id
-  if (users.length !== 1) {
-    // Add users to Users table
-    await knex("user_service.users").insert({
-      email: faker.internet.email(),
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      password: faker.internet.password()
-    });
-  }
+  if (!user) await knex("user_service.users").insert(createUser());
 
   // Create fake items
-  const fakeItems = [];
-  const fakeItemsCount = 1000;
+  const fakeItems: Array<IItem> = [];
+  const fakeItemsCount: number = 100;
 
   for (let i = 0; i < fakeItemsCount; i++) {
-    fakeItems.push(createFakeItem(userId));
+    fakeItems.push({
+      name: `${faker.commerce.productName()} v${i}`, // Add version suffix for uniqueness (constraint)
+      description: faker.commerce.product(),
+      created_by_user_id: userId,
+      last_modified_by_user_id: userId
+    });
   }
 
   // Add items to items table
   await knex("items_service.items").insert(fakeItems);
 
 }
-
-export { seed };
